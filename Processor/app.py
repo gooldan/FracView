e9e4035a7b0efb3e99c719a98780636be6851e35
Egor Shchavelev <egor5700@gmail.com>
2019-01-18 04:02:37 +0300

    post works

--- Processor/app.py
+++ Processor/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, url_for
+from flask import request, make_response, Response, jsonify
 from datetime import datetime
 from fractal import run_stub
 from random import random
@@ -7,9 +8,39 @@ import hashlib
 import os
 app = Flask(__name__)
 
+client = None
+
+import boto3
+client = boto3.client('s3')
+# Call S3 to list current buckets
+response = client.list_buckets()
+# Get a list of all bucket names from the response
+buckets = [bucket['Name'] for bucket in response['Buckets']]
+# Print out the bucket list
+print("DEBUG: Bucket List: %s" % buckets)
+
+class PrettyFloat(float):
+    def __repr__(self):
+        return '%.15g' % self
+
+def pretty_float(obj):
+    if isinstance(obj, float):
+        return PrettyFloat(obj)
+    elif isinstance(obj, str):
+        try:
+            return PrettyFloat(float(obj))
+        except ValueError:
+            return None
+    return None
+            
+    
+
 @app.route('/')
 def homepage():
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+    print ("CHECKING AMAZZON:")
+    print ("AWS_ACCESS_KEY_ID: ", os.environ.get("AWS_ACCESS_KEY_ID"))
+    print ("AWS_SECRET_ACCESS_KEY: ", os.environ.get("AWS_SECRET_ACCESS_KEY"))
 
     return """
     <h1>Hello heroku</h1>
@@ -36,13 +67,27 @@ def getstub():
 
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
     
-    the_path = "test.png"
     cwd = os.getcwd()
     print(cwd)
     file_name = "img_"+str(a)+"_"+str(b)+".png"
     img_name = "static/"+file_name
     run_stub(a+b*1j, img_name)
     url = url_for('static', filename=file_name)
+
+    import boto3
+    client = boto3.client('s3')
+
+    # Call S3 to list current buckets
+    response = client.list_buckets()
+
+    # Get a list of all bucket names from the response
+    buckets = [bucket['Name'] for bucket in response['Buckets']]
+
+    # Print out the bucket list
+    print("Bucket List: %s" % buckets)
+    client.upload_file(img_name, 'test-govno', file_name, ExtraArgs={'ACL':'public-read'})
+
+
     return """
     <h1>Hello heroku</h1>
     <p>It is currently {time}.</p>
@@ -50,5 +95,35 @@ def getstub():
     <img src="{url}">
     """.format(time=the_time, cwd = cwd, url = url)
 
+@app.route('/getfrac', methods = ['POST'])
+def getfrac():
+    if request.method == 'POST':
+        try:
+            body = request.json
+            a = pretty_float(body.get("a", None))
+            b = pretty_float(body.get("b", None))
+            if a is not None and b is not None:                
+                file_name = "img_"+str(a)+"_"+str(b)+".png"
+                img_name = "static/"+file_name
+                run_stub(a+b*1j, img_name)
+                client.upload_file(img_name, 'test-govno', file_name, ExtraArgs={'ACL':'public-read'})
+                resurl = "https://s3.amazonaws.com/test-govno/" + file_name
+                return jsonify(supurl = resurl)
+            else:
+                print("WARNING: Some data is empty. got body:", body, "a:", a, "b:", b)                
+        except KeyError as err:
+            print("WARNING: Bad post data, got body:", body, "err: ", err)
+        except AttributeError as err:
+            print("WARNING: Bad post data, got body:", body, "err: ", err)
+    else:
+        print("WARNING: Someone doing get request, ignoring")
+    return Response(status=201)
+
+
+
 if __name__ == '__main__':
+
+    
+
     app.run(debug=True, use_reloader=True)
+    
